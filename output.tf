3d39f7225eb45578e97a1f5bd40c9231417130b1
Amir Sezavar Keshavarz <1188298+amirsk@users.noreply.github.com>
2024-02-08 20:23:26 +0000

    Initial project to create a simple AWS Key Pair

---
+++ output.tf
@@ -0,0 +1,7 @@
+output "aws_key_pair_id" {
+  value = aws_key_pair.infra.id
+}
+
+output "aws_key_pair_arn" {
+  value = aws_key_pair.infra.arn
+}
---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  type        = string
+  description = "AWS Region"
+}
+
+variable "key_name" {
+  type        = string
+  description = "Key Name"
+}
+
+variable "public_key_path" {
+  type        = string
+  description = "Path to Public Key"
+}
